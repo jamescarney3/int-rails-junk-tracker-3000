a5432e642a9b19bcb4ec7c97d7ec4ef191c67bc9
James Carney <jmcarneyiii@gmail.com>
2021-04-23 17:39:35 -0400

    redo vehicle extension migration without type column

--- db/migrate/20210422200639_add_columns_to_vehicles.rb
+++ db/migrate/20210422200639_add_columns_to_vehicles.rb
@@ -2,7 +2,9 @@ class AddColumnsToVehicles < ActiveRecord::Migration[6.1]
   def change
     add_column :vehicles, :mileage, :integer
     add_column :vehicles, :registration_id, :string
-    add_column :vehicles, :vehicle_type, :string
     add_column :vehicles, :wheels, :integer
+    add_column :vehicles, :engine_status, :string
+    # not adding a type column here - will derive from specific
+    # type model implementations
   end
 end
