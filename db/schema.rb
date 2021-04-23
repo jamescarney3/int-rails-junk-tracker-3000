3bfabf2e5afa5433c7f2eb08945c3aa2b0d0cf63
James Carney <jmcarneyiii@gmail.com>
2021-04-23 17:55:21 -0400

    add migration to add coupes table and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_23_214800) do
+ActiveRecord::Schema.define(version: 2021_04_23_215438) do
+
+  create_table "coupes", force: :cascade do |t|
+    t.integer "doors"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "sedans", force: :cascade do |t|
     t.integer "doors"
