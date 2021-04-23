6e4c591c1e85a13bb887772ffcb429b822a09e36
James Carney <jmcarneyiii@gmail.com>
2021-04-23 17:47:21 -0400

    test vehicle model mileage rating and default engine status behavior

--- test/models/vehicle_test.rb
+++ test/models/vehicle_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class VehicleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "formats mileage rating" do
+    high_mileage = Vehicle.new mileage: 100001
+    medium_mileage = Vehicle.new mileage: 99999
+    low_mileage = Vehicle.new mileage: 9999
+    no_mileage = Vehicle.new
+
+    assert high_mileage.mileage_rating == 'high'
+    assert medium_mileage.mileage_rating == 'medium'
+    assert low_mileage.mileage_rating == 'low'
+    assert no_mileage.mileage_rating.nil?
+  end
+
+  test "defaults engine status to 'works'" do
+    new_vehicle = Vehicle.create
+    assert new_vehicle.engine_status == 'works'
+  end
 end
